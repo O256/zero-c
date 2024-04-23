4344fbd56bcd88c3e1e92ed584f006454786f9f0
oliver256 <5028994@qq.com>
2024-04-24 07:35:10 +0800

    拆分mbr和kernel

---
+++ kernel.asm
@@ -0,0 +1,31 @@
+; C0H0S2
+begin:
+    mov ax, 0xb800
+    mov ds, ax
+    mov [0x0000], byte 'H'
+    mov [0x0001], byte 0x0f
+    mov [0x0002], byte 'e'
+    mov [0x0003], byte 0x0f
+    mov [0x0004], byte 'l'
+    mov [0x0005], byte 0x0f
+    mov [0x0006], byte 'l'
+    mov [0x0007], byte 0x0f
+    mov [0x0008], byte 'o'
+    mov [0x0009], byte 0x0f
+    mov [0x000a], byte ' '
+    mov [0x000b], byte 0x0f
+    mov [0x000c], byte 'W'
+    mov [0x000d], byte 0x0f
+    mov [0x000e], byte 'o'
+    mov [0x000f], byte 0x0f
+    mov [0x0010], byte 'r'
+    mov [0x0011], byte 0x0f
+    mov [0x0012], byte 'l'
+    mov [0x0013], byte 0x0f
+    mov [0x0014], byte 'd'
+    mov [0x0015], byte 0x0f
+    mov [0x0016], byte '!'
+    mov [0x0017], byte 0x0f
+
+    hlt ; 暂停CPU
+    times 1024-($-begin) db 0 ; 填充剩余的空间
--- makefile
+++ makefile
@@ -2,12 +2,22 @@
 
 all: run
 
-run: mbr.bin
+run: a.img
 	bochs -qf bochsrc
 	# bochsdbg -qf bochsrc
 
 mbr.bin: mbr.asm
 	nasm -fbin mbr.asm -o mbr.bin
 
+kernel.bin: kernel.asm
+	nasm -fbin kernel.asm -o kernel.bin
+
+a.img: mbr.bin kernel.bin
+	dd if=/dev/zero of=a.img bs=512 count=3
+	dd if=mbr.bin of=a.img bs=512 count=1 conv=notrunc
+	dd if=kernel.bin of=a.img bs=512 count=1 seek=1 conv=notrunc
+
 clean:
 	-del -f mbr.bin
+	-del -f kernel.bin
+	-del -f a.img
