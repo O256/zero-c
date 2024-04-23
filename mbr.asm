31a401d0b2b9db253af0a67f95844666f1adb7fa
oliver256 <5028994@qq.com>
2024-04-23 08:12:55 +0800

    加载磁盘

--- mbr.asm
+++ mbr.asm
@@ -1,35 +1,61 @@
-; 默认为VGA模式，显示字符'H'
-; 80×25×16色， 一个字符占2个字节，一个字符对应一个字符单元，字符+颜色信息
-; 颜色信息：0-2位为背景色，4-6位为前景色，7位为闪烁，3位是否高亮
-; 对应的显存地址范围为0xb8000~0xb8f9f
-; 三大流行汇编风格：AT&T、Intel、NASM
-; 对应的汇编器分别位：gas、masm、nasm
-
-mov al, 0x03
-mov ah, 0x00
+; C0H0S1
+; 调用0x10号bios终端，清屏
+mov al, 0x03 ; 功能号
+mov ah, 0x00 ; 子功能号
 int 0x10
+; 加载一个扇区到0x08000的位置
+mov ax, 0x0800
+mov es, ax
+mov bx, 0x0000
+mov ah, 0x02 ; 功能号
+mov al, 0x02 ; 读取扇区数
+mov ch, 0x00 ; 柱面号
+mov dh, 0x00 ; 磁头号
+mov cl, 0x02 ; 扇区号
+mov dl, 0x00 ; 驱动器号
+int 0x13 ; 调用0x13号bios中断
 
-mov ax, 0xb800
-mov ds, ax
-mov [0x0000], byte 'H'
-mov [0x0001], byte 0x0f
+; 打印指定内存 x /64b 0x0800
+; 添加断点 pb
+; 查看寄存器 r 或者 sreg
 
-mov [0x0002], byte 'e'
-mov [0x0003], byte 0x8f
 
-mov [0x0004], byte 'l'
-mov [0x0005], byte 0x0f
-
-mov [0x0006], byte 'l'
-mov [0x0007], byte 0x0f
-
-mov [0x0008], byte 'o'
-mov [0x0009], byte 0x0f
-
-mov [0x000a], byte ' '
-mov [0x000b], byte 0x0f
-
-hlt
+; 跳转到0x0800:0x0000处执行
+jmp 0x0800:0x0000
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+; C0H0S2
+begin:  ; 代码段起始地址
+mov ax, 0xb800 ; 数据段起始地址
+mov ds, ax ; 数据段地址
+
+mov [0x0000], byte 'H'
+mov [0x0001], byte 0x0f
+mov [0x0002], byte 'e'
+mov [0x0003], byte 0x0f
+mov [0x0004], byte 'l'
+mov [0x0005], byte 0x0f
+mov [0x0006], byte 'l'
+mov [0x0007], byte 0x0f
+mov [0x0008], byte 'o'
+mov [0x0009], byte 0x0f
+mov [0x000a], byte ' '
+mov [0x000b], byte 0x0f
+mov [0x000c], byte 'W'
+mov [0x000d], byte 0x0f
+mov [0x000e], byte 'o'
+mov [0x000f], byte 0x0f
+mov [0x0010], byte 'r'
+mov [0x0011], byte 0x0f
+mov [0x0012], byte 'l'
+mov [0x0013], byte 0x0f
+mov [0x0014], byte 'd'
+mov [0x0015], byte 0x0f
+mov [0x0016], byte '!'
+mov [0x0017], byte 0x0f
+
+hlt
+
+times 1024-($-begin) db 0 ; 填充剩余空间
